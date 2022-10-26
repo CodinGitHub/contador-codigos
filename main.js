a6af7cd14c607e84f8f3c2772e13dee301b7d30e
CodinGitHub <gms2programador@gmail.com>
2022-10-26 05:49:06 -0500

    funcionalidad boton

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ let counterNumber = document.querySelector('.counter__number');
 
 let counter = 0;
 
-counterBtn.addEventListener('pressed', ()=>{
+counterBtn.addEventListener('mousedown', ()=>{
+    counterBtn.style.transform = 'translateY(4px)';
+});
+counterBtn.addEventListener('mouseup', ()=>{
     if(counter < 45){
         counter++
     }else{
         counter = 0;
     }
     counterNumber.innerText = counter; 
-    counterBtn.style.transform = 'translateY(4px)';
+    counterBtn.style.transform = 'translateY(0px)';
 });
-counterBtn.style.transform = 'translateY(0px)';
+counterBtn.addEventListener('mouseleave', ()=>{
+    counterBtn.style.transform = 'translateY(0px)';
+});
