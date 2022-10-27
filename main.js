2e5e94c9caec052e239aa014587cabf0c7d68f4a
David <gms2programador@gmail.com>
2022-10-26 21:59:39 -0500

    codigo con descripcion

--- main.js
+++ main.js
@@ -4,18 +4,43 @@ let counterNumber = document.querySelector('.counter__number');
 
 let counter = 0;
 
-counterBtn.addEventListener('mousedown', ()=>{
-    counterBtn.style.transform = 'translateY(4px)';
-});
-counterBtn.addEventListener('mouseup', ()=>{
+counterBtn.addEventListener('click', ()=>{
     if(counter < 45){
         counter++
     }else{
         counter = 0;
     }
     counterNumber.innerText = counter; 
-    counterBtn.style.transform = 'translateY(0px)';
 });
-counterBtn.addEventListener('mouseleave', ()=>{
-    counterBtn.style.transform = 'translateY(0px)';
+
+resetBtn.addEventListener('click', ()=>{
+    counter = 0;
+    counterNumber.innerText = counter; 
 });
+
+
+
+animateBtn(counterBtn)
+animateBtn(resetBtn)
+
+function animateBtn(btn){
+    btn.addEventListener('mouseup', ()=>{
+        btn.style.transform = 'translateY(0px)';
+    });
+
+    btn.addEventListener('mousedown', ()=>{
+        btn.style.transform = 'translateY(4px)';
+    });
+    
+    btn.addEventListener('mouseleave', ()=>{
+        btn.style.transform = 'translateY(0px)';
+    });
+
+    btn.addEventListener('touchstart', ()=>{
+        btn.style.transform = 'translateY(4px)';
+    });
+
+    btn.addEventListener('touchend', ()=>{
+        btn.style.transform = 'translateY(0px)';
+    });
+}
